6cfd7644688cb221a6d9769e10cb63ecb2a0904b
Ermes25 <ermes252020@gmail.com>
2024-04-08 16:49:28 -0600

    A.M.DB

--- base de datos/consultaperfumeria.sql
+++ base de datos/consultaperfumeria.sql
@@ -3,12 +3,12 @@ id_productos INT AUTO_INCREMENT PRIMARY KEY,
 NAME VARCHAR(200),
 precio double,
 descripcion VARCHAR(200),
-marca VARCHAR(200),
+marca VARCHAR(200)
 );
 
 CREATE TABLE categorias(
 id_categorias INT AUTO_INCREMENT PRIMARY KEY,
-NAME VARCHAR(200),
+NAME VARCHAR(200)
 );
 
 CREATE TABLE facturas(
@@ -17,5 +17,5 @@ id_categorias INT,
 id_productos INT,
 cantidad INT,
 FOREIGN KEY (id_productos) REFERENCES productos(id_productos),
-FOREIGN KEY (id_categorias) REFERENCES categorias(id_categorias),
+FOREIGN KEY (id_categorias) REFERENCES categorias(id_categorias)
 ); 
