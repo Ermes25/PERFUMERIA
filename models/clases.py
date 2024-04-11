57753185e46e9e70f15bfad401314f6fab2bb9ec
nahomybonilla <nahomybonilla02@gmail.com>
2024-04-11 14:37:31 -0600

    add

---
+++ models/clases.py
@@ -0,0 +1,40 @@
+class productos:
+    def __init__(self, name, precio, descripcion, marca , id):
+        self.name = name
+        self.precio = precio
+        self.descripcion = descripcion
+        self.marca = marca
+        self.id = id
+
+    def __str__(self):
+        return self.name
+    def get_price(self):
+        return self.precio
+    
+class categorias:
+    def __init__(self,name,sexo,id):
+        self.name = name
+        self.sexo = sexo
+        self.id = id
+    
+    def __str__(self):
+        return self.name
+
+
+class facturas:
+    def __init__(self,id_categorias,id_productos,cantidad,id):
+        self.id_categorias = id_categorias
+        self.id_productos = id_productos
+        self.cantidad = cantidad
+        self.id = id
+
+    def __str__(self):
+        return self.name
+    
+    def get_products(self):
+        return self.Productos.name    
+    
+    def get_categories(self):
+        return self.categorias.name
+    
+#class para los modelos
