4fe6da0238dc14990036df80114f94545da7cc56
Isamar23 <isamarmenaespinoza@gmail.com>
2024-04-13 08:20:27 -0600

    se modificaron bd

--- base de datos/consultaperfumeria.sql
+++ base de datos/consultaperfumeria.sql
@@ -1,14 +1,15 @@
 CREATE TABLE productos(
 id_productos INT AUTO_INCREMENT PRIMARY KEY,
-NAME VARCHAR(200),
+name VARCHAR(200),
 precio double,
 descripcion VARCHAR(200),
-marca VARCHAR(200),
+marca VARCHAR(200)
 );
 
 CREATE TABLE categorias(
 id_categorias INT AUTO_INCREMENT PRIMARY KEY,
-NAME VARCHAR(200),
+name VARCHAR(200),
+sexo VARCHAR(100)
 );
 
 CREATE TABLE facturas(
@@ -17,5 +18,6 @@ id_categorias INT,
 id_productos INT,
 cantidad INT,
 FOREIGN KEY (id_productos) REFERENCES productos(id_productos),
-FOREIGN KEY (id_categorias) REFERENCES categorias(id_categorias),
+FOREIGN KEY (id_categorias) REFERENCES categorias(id_categorias)
 ); 
+
